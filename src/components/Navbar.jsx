deea1c4df5861d02958ca74c8151686bcc69b022
Juan Carlos Gadea <87046066+JCGadeaDev@users.noreply.github.com>
2025-05-22 18:19:31 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function NavBar () {
         <nav className="navbar">
             <div className="container navbar-container">
                 <Link to="/" className="navbar-logo">
-                <h1>Star Wars May the forth</h1>
+                <h1>Star Wars May the force be with you</h1>
                 </Link>
                 <img src={starWarsIcon} alt="Star Wars Icon" className="navbar-icon" />
             </div>
